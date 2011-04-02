7646979e161df03789a986f9e81695eaa43f0846
yama.kyms <yama.kyms@e1fee2ac-5649-54b8-63f9-bfc2a7bd19d4>
2011-04-02 02:14:40 +0000

    v3 - admin/template/ディレクトリ更新

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -1,29 +1,28 @@
 <?php
-/****************************************************
-*
-* @File: 	footer.php
-* @Package:	GetSimple
-* @Action:	Template file for inserting the 
-*			footer into the control panel. 	
-*
-*****************************************************/
-
-global $i18n;
+/**
+ * Footer Admin Template
+ *
+ * @package GetSimple
+ */
 
 ?>
 		<div id="footer">
+      	<div class="footer-left" >
       	<?php 
       		include(GSADMININCPATH ."configuration.php");
       		if (cookie_check()) { 
-      			echo '<p><a href="pages.php">'.$i18n['PAGE_MANAGEMENT'].'</a> &nbsp;&bull;&nbsp; <a href="upload.php">'.$i18n['FILE_MANAGEMENT'].'</a> &nbsp;&bull;&nbsp; <a href="theme.php">'.$i18n['THEME_MANAGEMENT'].'</a> &nbsp;&bull;&nbsp; <a href="backups.php">'.$i18n['BAK_MANAGEMENT'].'</a> &nbsp;&bull;&nbsp; <a href="plugins.php">'.$i18n['PLUGINS_MANAGEMENT'].'</a> &nbsp;&bull;&nbsp; <a href="settings.php">'.$i18n['GENERAL_SETTINGS'].'</a> &nbsp;&bull;&nbsp; <a href="support.php">'.$i18n['SUPPORT'].'</a></p>';
+      			echo '<p><a href="pages.php">'.i18n_r('PAGE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="upload.php">'.i18n_r('FILE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="theme.php">'.i18n_r('THEME_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="backups.php">'.i18n_r('BAK_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="plugins.php">'.i18n_r('PLUGINS_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="settings.php">'.i18n_r('GENERAL_SETTINGS').'</a> &nbsp;&bull;&nbsp; <a href="support.php">'.i18n_r('SUPPORT').'</a></p>';
       		}
-      		$site_credit_link = stripslashes(' &nbsp;&bull;&nbsp; <a href="'.$site_link_back_url.'">'.$i18n['POWERED_BY'].' '.$site_full_name.'</a> '.$i18n['VERSION'].' '. $site_version_no);
-					echo '<p>'. sprintf($i18n['PRODUCTION'], '<a href="http://www.cagintranetworks.com/">Cagintranet Networks</a>') . $site_credit_link .'</p>';  
       	?>
-      	
+      		<p>&copy; 2009-<?php echo date('Y'); ?> <a href="http://get-simple.info/" target="_blank" >GetSimple CMS</a> &ndash; <?php echo i18n_r('VERSION') .' '. $site_version_no; ?></p>
+      	</div>
+      	<div class="gslogo" >
+	      	<a href="http://get-simple.info/" target="_blank" ><img src="template/images/getsimple_logo.gif" alt="GetSimple Content Management System" /></a>
+	      </div>
+      	<div class="clear"></div>
       	<?php exec_action('footer'); ?>
 
-		</div>
+		</div><!-- end #footer -->
 	</div><!-- end .wrapper -->
 </body>
 </html>
