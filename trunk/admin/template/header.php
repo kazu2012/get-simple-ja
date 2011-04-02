3c12f5e6f45c60259daaa41ca0e9aa6e19078f83
yama.kyms <yama.kyms@e1fee2ac-5649-54b8-63f9-bfc2a7bd19d4>
2011-04-02 02:14:40 +0000

    v3 - admin/template/ディレクトリ更新

--- trunk/admin/template/header.php
+++ trunk/admin/template/header.php
@@ -1,51 +1,62 @@
 <?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
-/****************************************************
-*
-* @File: 		header.php
-* @Package:	GetSimple
-* @Action:	Template file for inserting the 
-*						header into the control panel. 	
-*
-*****************************************************/
+/**
+ * Header Admin Template
+ *
+ * @package GetSimple
+ */
 
 global $LANG;
 $LANG_header = preg_replace('/(?:(?<=([a-z]{2}))).*/', '', $LANG);
-
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $LANG_header; ?>" lang="<?php echo $LANG_header; ?>">
-
+<!DOCTYPE html>
+<html lang="<?php echo $LANG_header; ?>">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"  />
 	<title><?php echo $title ?></title>
+	<link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+	<meta name="generator" content="GetSimple - <?php echo GSVERSION; ?>" /> 
+	<meta name="robots" content="noindex, nofollow">
 	
-	<!-- Javascript -->
-	<script type="text/javascript" src="template/js/jquery.min.js"></script>
-	<script type="text/javascript" src="template/js/uploadify/swfobject.js"></script>
-	<script type="text/javascript" src="template/js/uploadify/jquery.uploadify.js"></script>
-	<script type="text/javascript" src="template/js/facybox/jquery.facybox.js"></script>
-	<script type="text/javascript" src="template/js/jquery.reverseorder.js"></script>
+	
+	<!-- iOS meta tags to optimize experience -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link rel="apple-touch-icon" href="apple-touch-icon.png"/>
+	
+	
+	<!-- Javascript Plugins -->
+	<script type="text/javascript" src="template/js/jquery.min.js?v=1.5.1"></script>
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<?php if( ((get_filename_id()=='upload') || (get_filename_id()=='image')) && (!defined('GSNOUPLOADIFY')) ) { ?>
+	<script type="text/javascript" src="template/js/uploadify/jquery.uploadify.js?v=3.0"></script>
+	<?php } ?>
+	<?php if(defined('GSPAGER')) { ?>
 	<script type="text/javascript" src="template/js/jquery.quickpaginate.js"></script>
-	<script type="text/javascript" src="template/js/jquery.example.min.js"></script>
+	<?php } ?>	
+	<?php if(get_filename_id()=='image') { ?>
 	<script type="text/javascript" src="template/js/jcrop/jquery.Jcrop.min.js"></script>
-	<script type="text/javascript" src="template/js/jquery.getsimple.js"></script>
-	
-	<!-- CSS Stylesheets -->
-	<link rel="stylesheet" type="text/css" href="template/style.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="template/js/facybox/jquery.facybox.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="template/js/jcrop/jquery.Jcrop.css" media="screen" />
-	<!--[if IE 6]><link rel="stylesheet" type="text/css" href="template/ie6.css" media="screen" /><![endif]-->
+	<?php } ?>
+	<?php if(get_filename_id()=='edit') { ?>
+	<noscript><style>#metadata_window {display:block !important} </style></noscript>
+	<?php } ?>
 	
-	<!-- IE Fixes -->
-	<script type="text/javascript"><!--
-		try {
-			document.execCommand("BackgroundImageCache", false, true);
-		} catch(err) {}
-		/* IE6 flicker hack from http://dean.edwards.name/my/flicker.html */
-	--></script>
-	<noscript><style type="text/css">#metadata_window {display:block !important} </style></noscript>
+	<script type="text/javascript" src="template/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+	<link rel="stylesheet" type="text/css" href="template/js/fancybox/jquery.fancybox-1.3.4.css" media="screen" />		
+
+
+	<!-- GetSimple specific files -->
+	<script type="text/javascript" src="template/js/jquery.getsimple.js?v=<?php echo GSVERSION; ?>"></script>
+	<link rel="stylesheet" type="text/css" href="template/style.php?v=<?php echo GSVERSION; ?>" media="screen" />
+	<!--[if IE 6]><link rel="stylesheet" type="text/css" href="template/ie6.css?v=<?php echo GSVERSION; ?>" media="screen" />
+	<![endif]-->
 	
-	<?php exec_action('header'); ?>
+	<?php 
+		# Plugin hook to allow insertion of stuff into the header
+		exec_action('header'); 
+	?>
 	
 </head>
 
--- trunk/admin/template/include-nav.php
+++ trunk/admin/template/include-nav.php
@@ -1,40 +1,45 @@
 <?php
-/****************************************************
-*
-* @File: 		include-nav.php
-* @Package:	GetSimple
-* @Action:	Template file for inserting the top navigation into the control panel. 	
-*
-*****************************************************/
-?>
-
-<?php if (cookie_check()) { 
-		echo '<ul id="pill"><li class="leftnav"><a href="logout.php" accesskey="l" >'.$i18n['TAB_LOGOUT'].'</a></li>';
-		if (defined('GSDEBUG')) {
-			echo '<li class="debug"><a href="http://get-simple.info/theme-developer-tips" target="_blank">DEBUG MODE</a></li>';
-		}
-		echo '<li class="rightnav" ><a href="settings.php#profile">'.$i18n['WELCOME'].' <b>'.$USR.'</b>!</a></li></ul>'; 
+/**
+ * Navigation Include Template
+ *
+ * @package GetSimple
+ */
+ 
+if (cookie_check()) { 
+	echo '<ul id="pill"><li class="leftnav"><a href="logout.php" accesskey="'. find_accesskey(i18n_r('TAB_LOGOUT')).'" >'.i18n_r('TAB_LOGOUT').'</a></li>';
+	if (defined('GSDEBUG')) {
+		echo '<li class="debug"><a href="http://get-simple.info/wiki/themes:tips" target="_blank">'.i18n_r('DEBUG_MODE').'</a></li>';
+	}
+	echo '<li class="rightnav" ><a href="settings.php#profile">'.i18n_r('WELCOME').' <b>'.$USR.'</b>!</a></li></ul>';
 } 
 
 //determine page type if plugin is being shown
 if (get_filename_id() == 'load') {
 	$plugin_class = $plugin_info[$plugin_id]['page_type'];
+} else {
+	$plugin_class = '';
 }
 
 ?>
 
-<ul class="nav <?php echo @$plugin_class; ?>">
-	<li><a class="pages" href="pages.php" accesskey="p" ><?php echo $i18n['TAB_PAGES'];?></a></li>
-	<li><a class="files" href="upload.php" accesskey="f" ><?php echo $i18n['TAB_FILES'];?></a></li>
-	<li><a class="theme" href="theme.php" accesskey="t" ><?php echo $i18n['TAB_THEME'];?></a></li>
-	<li><a class="backups" href="backups.php" accesskey="b" ><?php echo $i18n['TAB_BACKUPS'];?></a></li>
-	<li><a class="plugins" href="plugins.php" accesskey="n" ><?php echo $i18n['PLUGINS_NAV'];?></a></li>
+<ul class="nav <?php echo $plugin_class; ?>">
+	<li><a class="pages" href="pages.php" accesskey="<?php echo find_accesskey(i18n_r('TAB_PAGES'));?>" ><?php i18n('TAB_PAGES');?></a></li>
+	<li><a class="files" href="upload.php" accesskey="<?php echo find_accesskey(i18n_r('TAB_FILES'));?>" ><?php i18n('TAB_FILES');?></a></li>
+	<li><a class="theme" href="theme.php" accesskey="<?php echo find_accesskey(i18n_r('TAB_THEME'));?>" ><?php i18n('TAB_THEME');?></a></li>
+	<li><a class="backups" href="backups.php" accesskey="<?php echo find_accesskey(i18n_r('TAB_BACKUPS'));?>" ><?php i18n('TAB_BACKUPS');?></a></li>
+	<li><a class="plugins" href="plugins.php" accesskey="<?php echo find_accesskey(i18n_r('PLUGINS_NAV'));?>" ><?php i18n('PLUGINS_NAV');?></a></li>
 	
 	<?php exec_action('nav-tab');	?>
 	
-	<li><img class="toggle" id="loader" src="template/images/ajax.gif" alt=""/></li>
-	<li class="rightnav" ><a class="settings first" href="settings.php" accesskey="s" ><?php echo $i18n['TAB_SETTINGS'];?></a></li>
-	<li class="rightnav" ><a class="support last" href="support.php" accesskey="o" ><?php echo $i18n['TAB_SUPPORT'];?></a></li>
+	<li><img class="toggle" id="loader" src="template/images/ajax.gif" alt="" /></li>
+	<li class="rightnav" ><a class="settings first" href="settings.php" accesskey="<?php echo find_accesskey(i18n_r('TAB_SETTINGS'));?>" ><?php i18n('TAB_SETTINGS');?></a></li>
+	<li class="rightnav" ><a class="support last" href="support.php" accesskey="<?php echo find_accesskey(i18n_r('TAB_SUPPORT'));?>" ><?php i18n('TAB_SUPPORT');?></a></li>
+	
+	<!--
+	 add this to any .nav <li> element to display a warning symbol on it. 
+	 This will primarily be used when an update is needed or something is determined 'wrong'
+	 code: <span class="warning">!</span>
+	-->
 </ul>
 <div class="clear" ></div>
 </div>
